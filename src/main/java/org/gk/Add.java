d552023d23f053ea8ed7e8ffe71c654f0eb4f949
Georgiy Koptelov <kogeor@ya.ru>
2022-09-26 09:53:38 +0300

    Add

--- src/main/java/org/gk/Add.java
+++ src/main/java/org/gk/Add.java
@@ -9,24 +9,23 @@ import java.io.PrintWriter;
 
 @WebServlet(name = "Add", value = "/add")
 public class Add extends HttpServlet {
-    private String message;
 
-    public void init() {
-        message = "Hello World!";
-    }
+    MyHashMap myHashMap = MyHashMap.getInstance();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
+
         out.println("</body></html>");
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        String key = request.getParameter("key");
+        String value = request.getParameter("value");
+        myHashMap.put(key, value);
     }
 
     public void destroy() {
---
+++ src/main/webapp/add.jsp
@@ -0,0 +1,27 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Kogeor
+  Date: 26-Sep-22
+  Time: 09:40
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Add element</title>
+</head>
+<body>
+<div>
+  <form method="post">
+    <label>Key:
+      <input type="text" name="key"><br />
+    </label>
+
+    <label>Value:
+      <input type="text" name="value"><br />
+    </label>
+    <button type="submit">Submit</button>
+  </form>
+</div>>
+</body>
+</html>
